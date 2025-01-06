9170dfa33366a9fabc7ca44eecc44e4c15e15f8b
arkadeep-baksi <rik.21091997@gmail.com>
2025-01-06 22:25:00 +0530

    Completed assignment: generating, decoding and verifying jwt Token

--- assignments/week-3/02-jwt/index.js
+++ assignments/week-3/02-jwt/index.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
-
+const zod = require("zod");
 
 /**
  * Generates a JWT for a given username and password.
@@ -15,6 +15,21 @@ const jwtPassword = 'secret';
  */
 function signJwt(username, password) {
     // Your code here
+    const User = zod.object({
+        username : zod.string().email(),
+        password : zod.string().min(6)
+    });
+    const validation = User.safeParse({
+        username : username,
+        password : password
+    });
+    if (!validation.success) {
+        return null;
+    }
+    else{
+        const jwtToken = jwt.sign({username : username}, jwtPassword);
+        return jwtToken;
+    }
 }
 
 /**
@@ -27,6 +42,14 @@ function signJwt(username, password) {
  */
 function verifyJwt(token) {
     // Your code here
+    // jwt.verify returns the verified string if the token can be verified, else throws error
+    try{
+        const decode = jwt.verify(token, jwtPassword);
+        return true;
+    } catch(err){
+        return false;
+    }
+
 }
 
 /**
@@ -38,8 +61,16 @@ function verifyJwt(token) {
  */
 function decodeJwt(token) {
     // Your code here
-}
+    // jwt.decode returns the decoded object or null if it is not a valid jwt token
+    const decoded = jwt.decode(token);
+    if (decoded){
+        return true;
+    }
+    else{
+        return false;
+    }
 
+}
 
 module.exports = {
   signJwt,
