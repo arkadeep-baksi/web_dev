c9bacbeba029c69df5344b803c01e878c6da0268
arkadeep-baksi <rik.21091997@gmail.com>
2025-01-11 22:19:37 +0530

    Adding the Schemas for Admin, Users, Courses

--- assignments/week-3/03-mongo/db/index.js
+++ assignments/week-3/03-mongo/db/index.js
@@ -1,27 +1,49 @@
-const mongoose = require('mongoose');
+// Import mongoose, zod
+const zod = require("zod");
+const mongoose = require("mongoose");
 
-// Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+// Connect to the Mongodb Database
+mongoose.connect("mongodb+srv://rik21091997:149AXGEHNISEnQAR@cluster0.g26c4.mongodb.net/courses?retryWrites=true&w=majority&appName=Cluster0",);
 
-// Define schemas
-const AdminSchema = new mongoose.Schema({
-    // Schema definition here
-});
+// Define the schemas
+
+const adminSchema = new mongoose.Schema({
+    username: String,
+    password: String
+})
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
-});
+    username: String,
+    password: String,
+    purchasedCourses: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'courses'
+    }
+})
 
-const CourseSchema = new mongoose.Schema({
-    // Schema definition here
-});
+const courseSchema = new mongoose.Schema({
+    name: String,
+    price: parseFloat
+})
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+// const AdminSchema = new mongoose.Schema({
+//     // Schema definition here
+// });
 
-module.exports = {
-    Admin,
-    User,
-    Course
-}
+// const UserSchema = new mongoose.Schema({
+//     // Schema definition here
+// });
+
+// const CourseSchema = new mongoose.Schema({
+//     // Schema definition here
+// });
+
+// const Admin = mongoose.model('Admin', AdminSchema);
+// const User = mongoose.model('User', UserSchema);
+// const Course = mongoose.model('Course', CourseSchema);
+
+// module.exports = {
+//     Admin,
+//     User,
+//     Course
+// }
