481aa10b2ed8fdda75d1136901e583a5b39d365e
arkadeep-baksi <rik.21091997@gmail.com>
2025-01-14 18:51:06 +0530

    Updated all the schemas with the collection name they should refer to

--- assignments/week-3/03-mongo/db/index.js
+++ assignments/week-3/03-mongo/db/index.js
@@ -7,34 +7,35 @@ const { MongoParseError } = require("mongodb");
 mongoose.connect("mongodb+srv://rik21091997:149AXGEHNISEnQAR@cluster0.g26c4.mongodb.net/courses?retryWrites=true&w=majority&appName=Cluster0",);
 
 // Define the schemas
+// By default, Mongoose uses the pluralized, lowercase version of youre Model name as the table name
+// If your collection name doesn't follow the same pattern, mention the collection/table name in the Schema itself
 
 const adminSchema = new mongoose.Schema({
     username: String,
     password: String
-})
+}, {collection : 'Admins'})
 
 const UserSchema = new mongoose.Schema({
     username: String,
     password: String,
     purchasedCourses: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'courses'
+        ref: 'Courses'
     }
-})
+}, {collection : 'Users'})
 
 const courseSchema = new mongoose.Schema({
     title: String,
     description: String,
     price: Number,
     imageLink: String
-})
+}, {collection : 'Courses'})
 
-const Admin = mongoose.model('Admin', adminSchema);
+const Admin = mongoose.model('Admins', adminSchema);
 const User = mongoose.model('Users', UserSchema);
 const Course = mongoose.model('Courses', courseSchema);
 
 module.exports = {
-
     Admin,
     User,
     Course
