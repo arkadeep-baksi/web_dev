dd3a73f611252a663cd3349987dea117469502d1
arkadeep-baksi <rik.21091997@gmail.com>
2025-01-15 22:32:54 +0530

    Added middlewares with JET verification for users and admins

--- assignments/week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ assignments/week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,20 @@
+// Middleware for handling auth
+const jwt = require("jsonwebtoken");
+const {JWT_Secret} = require("../db");
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    
+    const token = req.headers.authorization;
+    // Remove the Bearer word
+    const jwtToken = token.split(" ")[1]; 
+    const decodedValue = jwt.verify(jwtToken, JWT_Secret);
+    if (decodedValue.username){
+        next();
+    }else{
+        res.status(403).json({
+            "mssg": "You are not authenticated !!"
+        });
+    }
 }
 
 module.exports = userMiddleware;
