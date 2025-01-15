c02e4d331b5145bf4bc41da1873108c8741a4d95
arkadeep-baksi <rik.21091997@gmail.com>
2025-01-15 22:33:50 +0530

    Added all routes for users and admins

--- assignments/week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ assignments/week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,101 @@
+const jwt = require("jsonwebtoken")
 const { Router } = require("express");
+const bodyParser = require("body-parser");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
+const { Course } = require("../db");
+const { JWT_Secret } = require("../db");
 const router = Router();
 
+router.use(bodyParser.json());
+
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
-});
+router.post('/signup', async (req, res) => {
+    
+    const username = req.body.username;
+    const password = req.body.password;
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
-});
+    const admin = await Admin.findOne({
+        username : username,
+        password : password
+    });
+    if (admin){
+        res.json({
+            "mssg" : "Admin account already exists !!"
+        });
+    }
+    else{
+        await Admin.create({
+            username : username,
+            password : password
+        });
+        res.status(200).json({
+            "mssg" : "Admin account created successfully !!"
+        });
+    }
 
-router.post('/courses', adminMiddleware, (req, res) => {
+})
+
+router.post('/signin', async (req, res) => {
+    // Implement admin signup logic
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const admin = await Admin.findOne({
+        username : username,
+        password : password
+    });
+
+    if (admin){
+
+        const jwt = jwt.sign({username}, JWT_Secret);
+        res.status(200).json({
+            "token" : "Bearer" + " " + jwt
+        });
+        
+    }else{
+        res.status(411).json({
+            "mssg" : "Invalid username or password !!"
+        });
+    }
+
+})
+
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
-});
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageLink = req.body.imageLink;
 
-router.get('/courses', adminMiddleware, (req, res) => {
+    const value = await Course.create({
+        title:  title,
+        description: description,
+        price: price,
+        imageLink: imageLink
+    });
+
+    res.status(200).json({
+        "message": 'Course created successfully',
+        "courseId": value._id
+    });
+})
+
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
-});
+
+    const value = await Course.find({});
+    res.status(200).json({
+        "course": value
+    });
+})
+
+router.use(function(err, req, res, next){
+
+    console.log(err)
+    res.status(500).send("Internal Server Error");
+    
+})
 
 module.exports = router;
--- assignments/week-3/04-mongo-with-jwt-auth/routes/user.js
+++ assignments/week-3/04-mongo-with-jwt-auth/routes/user.js
@@ -1,26 +1,121 @@
+const jwt = require("jsonwebtoken")
 const { Router } = require("express");
-const router = Router();
+const bodyParser = require("body-parser");
 const userMiddleware = require("../middleware/user");
+const { User } = require("../db");
+const { Course } = require("../db");
+const { JWT_Secret } = require("../db");
+const router = Router();
+
+router.use(bodyParser.json());
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post('/signup', async (req, res) => {
+    
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+        username : username,
+        password : password
+    });
+    if (user){
+        res.json({
+            "mssg" : "User account already exists !!"
+        });
+    }
+    else{
+        await User.create({
+            username : username,
+            password : password
+        });
+        res.status(200).json({
+            "mssg" : "User account created successfully !!"
+        });
+    }
+
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
-});
+router.post('/signin', async (req, res) => {
+    
+    const username = req.body.username;
+    const password = req.body.password;
 
-router.get('/courses', (req, res) => {
+    const user = await User.findOne({
+        username : username,
+        password : password
+    });
+
+    if (user){
+
+        const jwt = jwt.sign({username}, JWT_Secret);
+        res.status(200).json({
+            "token" : "Bearer" + " " + jwt
+        });
+        
+    }else{
+        res.status(411).json({
+            "mssg" : "Invalid username or password !!"
+        });
+    }
+
+})
+
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
-});
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+    const value = await Course.find({});
+    res.status(200).json({
+        "course": value
+    });
+
+})
+
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
     // Implement course purchase logic
-});
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+    const courseId = req.params.courseId;
+    const username = req.headers.username;
+
+    await User.updateOne({
+        username : username
+    }, {
+        "$push" : {
+            purchasedCourses : courseId
+        }
+    });
+
+    res.status(200).json({
+        "mssg" : "Course Purchased successfully !!"
+    });
+
+
+})
+
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
-});
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    const value = await User.findOne({
+        username : username
+    });
+    
+    const response = await Course.find({
+        _id : {
+            "$in" : value.purchasedCourses
+        }
+    })
+    res.status(200).json({
+        "courses" : response
+    });
+})
+
+router.use(function(err, req, res, next){
+
+    console.log(err)
+    res.status(500).send("Internal Server Error");
+    
+})
 
 module.exports = router
