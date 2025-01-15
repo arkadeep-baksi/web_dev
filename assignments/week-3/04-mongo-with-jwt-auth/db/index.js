cf9c78ed73ab018f08211d2b53acd82e747186a9
arkadeep-baksi <rik.21091997@gmail.com>
2025-01-15 22:32:15 +0530

    Adding all the Admin, User, Course Schemas

--- assignments/week-3/04-mongo-with-jwt-auth/db/index.js
+++ assignments/week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,27 +1,44 @@
-const mongoose = require('mongoose');
+// Import mongoose, zod
+const zod = require("zod");
+const mongoose = require("mongoose");
+const { MongoParseError } = require("mongodb");
+const JWT_Secret = "arkadeep@jwt"
 
-// Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+// Connect to the Mongodb Database
+mongoose.connect("mongodb+srv://rik21091997:149AXGEHNISEnQAR@cluster0.g26c4.mongodb.net/courses?retryWrites=true&w=majority&appName=Cluster0",);
 
-// Define schemas
-const AdminSchema = new mongoose.Schema({
-    // Schema definition here
-});
+// Define the schemas
+// By default, Mongoose uses the pluralized, lowercase version of youre Model name as the table name
+// If your collection name doesn't follow the same pattern, mention the collection/table name in the Schema itself
+
+const adminSchema = new mongoose.Schema({
+    username: String,
+    password: String
+}, {collection : 'Admins'})
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
-});
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Courses'
+    }]
+} , {collection : 'Users'})
 
-const CourseSchema = new mongoose.Schema({
-    // Schema definition here
-});
+const courseSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String
+}, {collection : 'Courses'})
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model('Admins', adminSchema);
+const User = mongoose.model('Users', UserSchema);
+const Course = mongoose.model('Courses', courseSchema);
 
 module.exports = {
     Admin,
     User,
-    Course
+    Course,
+    JWT_Secret
 }
