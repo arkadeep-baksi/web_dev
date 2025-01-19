afb23d77de3f830b4bc8a7683b96ad7c4d83a8ac
arkadeep-baksi <rik.21091997@gmail.com>
2025-01-19 18:58:21 +0530

    Counting vowels in case insensitive fashion

--- assignments/01-js/medium/countVowels.js
+++ assignments/01-js/medium/countVowels.js
@@ -5,8 +5,19 @@
   Once you've implemented the logic, test your code by running
 */
 
-function countVowels(str) {
-    // Your code here
+function countVowels(str1) {
+    let count = 0;
+    str1 = str1.toLowerCase();
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    for (let i = 0; i<str1.length; i++){
+      
+      let char = str1.charAt(i);
+      if (vowels.indexOf(char) !== -1){
+        count+=1;
+      }
+    }
+    return count;
 }
 
 module.exports = countVowels;
