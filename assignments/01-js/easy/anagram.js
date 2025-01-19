154de3cf0df161be8723cdaebaae12ea071b3b70
arkadeep-baksi <rik.21091997@gmail.com>
2025-01-19 17:23:24 +0530

    Function to check if two strings are anagrams or not

--- assignments/01-js/easy/anagram.js
+++ assignments/01-js/easy/anagram.js
@@ -6,6 +6,33 @@
 
 function isAnagram(str1, str2) {
 
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  if (str1.length != str2.length){
+    return false;
+  }
+  // Array to keep a frequency count of the characters of any one of the string
+  const chars = new Array(128).fill(0);
+
+  for (let i = 0; i< str1.length; i++){
+    chars[str1.charCodeAt(i)] +=1;
+  }
+  for (let i = 0 ;i<str2.length; i++){
+    if (!chars[str2.charCodeAt(i)]){
+      return false;
+    }
+    else{
+      chars[str2.charCodeAt(i)]-=1;
+    }
+  }
+  // Checking if the frequency of each unique character exactly matches
+  for (let i = 0; i<chars.length; i++){
+    if (chars[i]){
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
