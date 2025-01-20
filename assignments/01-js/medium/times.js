5ab7bb7a36542c80d1f942484151319324444e87
arkadeep-baksi <rik.21091997@gmail.com>
2025-01-20 23:36:41 +0530

    Code for calculating time of various operations

--- assignments/01-js/medium/times.js
+++ assignments/01-js/medium/times.js
@@ -9,5 +9,16 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    
+    const startTime = Date.now();
+    let sum = 0;
+    for (let i = 1; i<=n; i++){
+        sum+=i
+    }
+    const timeElapsed = (Date.now()-startTime)/1000; // Return time in seconds
+    return timeElapsed;
 }
+
+console.log(`Time taken for sum from 1 to 100: ${calculateTime(100)} seconds`);
+console.log(`Time taken for sum from 1 to 100000: ${calculateTime(100000)} seconds`);
+console.log(`Time taken for sum from 1 to 1000000000: ${calculateTime(1000000000)} seconds`);
